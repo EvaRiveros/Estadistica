86daef02f61940a2a2a0780ab7202430589f99c4
EvaRiveros <eva.riveros.o@gmail.com>
2016-06-20 12:41:22 -0300

    Add files via upload

--- I3.py
+++ I3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import batman
-from pymc3 import Model, Normal
+from pymc3 import Model, Normal, HalfNormal, Uniform
 
 t,target,f0,f1,f2,f3,f4,f5,f6,f7,f8=np.loadtxt('dataset_11.dat', skiprows=1, unpack=True)
 N=np.size(f0)
@@ -53,8 +53,7 @@ for i in xrange(0,p):
 # plt.plot(t,np.dot(A[:,0],finaldata)) #modelo log(flujo) estrella 0
 # plt.plot(t,F[:,0],'+') #log(flujo) real estrella 0 
 
-# print valores propios
-
+print 'Valores propios:'
 print eigval
 
 # plotear componentes S
@@ -69,45 +68,63 @@ plt.plot(t,finaldata[6,:])
 plt.plot(t,finaldata[7,:])
 plt.plot(t,finaldata[8,:])
 
+plt.title('componentes')
+plt.xlabel('tiempo')
 plt.plot()
 plt.show()
 
 
 ########################### estimacion constante ########################################
 
-atarget=np.mean(A,axis=1) #estimacion coeficientes alfa para la estrella objetivo
+a_est=np.mean(A,axis=1) #estimacion coeficientes alfa para la estrella objetivo
 t_st=t[np.logical_or(t>1.,t<-1.)] #vector tiempo sin transito
 target_st=target[np.logical_or(t>1.,t<-1.)] #flujo estrella objetivo sin transito
 
-c_est=np.mean(target_st-np.dot(atarget,finaldata)[np.logical_or(t>1.,t<-1.)]) #0.007+-0.05
+c_est=np.mean(target_st-np.dot(a_est,finaldata)[np.logical_or(t>1.,t<-1.)]) #0.007+-0.05
 
 #########################################################################################
 
-
-params = batman.TransitParams()
-params.t0 = 0.                       #time of inferior conjunction
-params.per = 0.78884                 #orbital period
-params.rp = rp                       #planet radius (in units of stellar radii)
-params.a = a                         #semi-major axis (in units of stellar radii)
-params.inc = i                       #orbital inclination (in degrees)
-params.ecc = 0.                      #eccentricity
-params.w = 90.                       #longitude of periastron (in degrees)
-params.u = [0.1, 0.3]                #limb darkening coefficients
-params.limb_dark = "quadratic"       #limb darkening model
-
-t = np.linspace(-2,2.5,100)
-
-m = batman.TransitModel(params,t)
-flux = m.light_curve(params)
-
-
-#########################################################################################
 
 basic_model=Model()
+
+alfasd=(np.amax(a_est)-np.amin(a_est)/2.)
 
 with basic_model:
 	# priors parametros
 	c=Normal('constante',mu=c_est,sd=0.05)
-	
-	
+	alfa0=Normal('coeficiente0',mu=a_est[0],sd=alfasd)
+	alfa1=Normal('coeficiente1',mu=a_est[1],sd=alfasd)
+	alfa2=Normal('coeficiente2',mu=a_est[2],sd=alfasd)
+	alfa3=Normal('coeficiente3',mu=a_est[3],sd=alfasd)
+	alfa4=Normal('coeficiente4',mu=a_est[4],sd=alfasd)
+	alfa5=Normal('coeficiente5',mu=a_est[5],sd=alfasd)
+	alfa6=Normal('coeficiente6',mu=a_est[6],sd=alfasd)
+	alfa7=Normal('coeficiente7',mu=a_est[7],sd=alfasd)
+	alfa8=Normal('coeficiente8',mu=a_est[8],sd=alfasd)# cantidad de alfas depende de p
+	sigma=HalfNormal('sigma',sd=1)
+	rp=Uniform('rp',lower=0,upper=1)
+	a=Uniform('semi-major axis',lower=0,upper=500)
+	i=Uniform('inclination',lower=0,upper=80)
 
+	suma=alfa0*finaldata[0,:]+alfa1*finaldata[1,:]+alfa2*finaldata[2,:]+alfa3*finaldata[3,:]+alfa4*finaldata[4,:]+alfa5*finaldata[5,:]+alfa6*finaldata[6,:]+alfa7*finaldata[7,:]+alfa8*finaldata[8,:]
+	mu=c+np.log(transito(rp,a,i))+suma
+	logF=Normal('Flujo estrella objetivo',mu=mu,sd=sigma,observed=target)
+
+#########################################################################################
+
+def transito(rp,a,i):
+	params = batman.TransitParams()
+	params.t0 = 0.                       #time of inferior conjunction
+	params.per = 0.78884                 #orbital period
+	params.rp = rp                       #planet radius (in units of stellar radii)
+	params.a = a                         #semi-major axis (in units of stellar radii)
+	params.inc = i                       #orbital inclination (in degrees)
+	params.ecc = 0.                      #eccentricity
+	params.w = 90.                       #longitude of periastron (in degrees)
+	params.u = [0.1, 0.3]                #limb darkening coefficients
+	params.limb_dark = "quadratic"       #limb darkening model
+
+	t_t = np.linspace(-2,2.5,100)
+
+	m = batman.TransitModel(params,t_t)
+	return flux = m.light_curve(params)
